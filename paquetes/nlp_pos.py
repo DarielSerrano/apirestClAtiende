3d8facc4b03d9eac4942d74b5213531fac226914
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-07-20 14:16:53 -0400

    s

--- paquetes/nlp_pos.py
+++ paquetes/nlp_pos.py
@@ -32,32 +32,36 @@ import os
 # Establecer la variable de entorno con la ubicación del archivo de credenciales
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../key/pragmatic-byway-393406-ab498ee30544.json"
 
-def classify(text, verbose=True):
-    """Classify the input text into categories."""
+from google.cloud.language_v1 import enums
 
-    language_client = language_v1.LanguageServiceClient()
+def extract_verbs_and_nouns(text):
+    client = language_v1.LanguageServiceClient()
 
-    document = language_v1.Document(
-        content=text, type_=language_v1.Document.Type.PLAIN_TEXT
-    )
-    response = language_client.classify_text(request={"document": document})
-    categories = response.categories
+    # Configurar el tipo de análisis
+    type_ = enums.Document.Type.PLAIN_TEXT
+    language = "es"
+    document = {"content": text, "type": type_, "language": language}
 
-    result = {}
+    # Realizar la solicitud de análisis
+    response = client.analyze_syntax(document=document)
 
-    for category in categories:
-        # Turn the categories into a dictionary of the form:
-        # {category.name: category.confidence}, so that they can
-        # be treated as a sparse vector.
-        result[category.name] = category.confidence
+    # Extraer verbos y sustantivos del resultado
+    verbs = []
+    nouns = []
+    for token in response.tokens:
+        if token.part_of_speech.tag == enums.PartOfSpeech.Tag.VERB:
+            verbs.append(token.text.content)
+        elif token.part_of_speech.tag == enums.PartOfSpeech.Tag.NOUN:
+            nouns.append(token.text.content)
 
-    if verbose:
-        print(text)
-        for category in categories:
-            print("=" * 20)
-            print("{:<16}: {}".format("category", category.name))
-            print("{:<16}: {}".format("confidence", category.confidence))
+    return verbs, nouns
 
-    return result
+# Ejemplo de texto a analizar
+texto_a_analizar = "Hola, soy un ejemplo de texto en español."
 
-classify("Hola, soy un ejemplo de texto en español. Estoy muy feliz y quiero compartirlo contigo. Google Cloud Natural Language API es increíble.")
+# Realizar el análisis y obtener los verbos y sustantivos
+verbs, nouns = extract_verbs_and_nouns(texto_a_analizar)
+
+# Imprimir los resultados
+print("Verbos:", verbs)
+print("Sustantivos:", nouns)
