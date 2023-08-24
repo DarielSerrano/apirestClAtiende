f13efd246e6caf86f4c699e76363c48da513ada2
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-24 19:07:44 -0400

    fix extraer

--- paquetes/extraer.py
+++ paquetes/extraer.py
@@ -30,7 +30,6 @@ nlp_ner = pipeline("ner", model=model, tokenizer=tokenizer)
 
 # Leer el contenido del archivo pasado como argumento en la línea de comandos
 text = (file_get_contents(sys.argv[1]))
-# text = "El origen de este proyecto surge de la inquietud presentada el día 13 de julio del 2022 por la Sra. Rosa Bravo y el Sr. Carlos Salgado Jefe Subdepartamento Gestión de la Innovación, Departamento Gestión Estratégica y Estudios, División Planificación y Desarrollo para trabajar en proyectos junto a la Universidad.  El Instituto de Previsión Social (IPS) es un organismo  del estado cuya misión es la entrega beneficios y servicios previsionales y sociales, a través de su red de atención ChileAtiende, promoviendo la excelencia en su gestión y acercando de manera inclusiva el Estado a las personas, considerando a sus funcionarios y funcionarias como el principal capital de la institución. ChileAtiende es una red administrada por el IPS que busca acercar los servicios del Estado a las personas, entregando información de un conjunto de trámites de diferentes instituciones públicas, en un solo lugar. El Portal provee información acerca de trámites, leyes, beneficios, procedimientos, entre otros documentos de más de 29 instituciones públicas, incluyendo al propio IPS.  La atención a clientes se realiza a través de distintos canales de información, tales como sitio web, sucursales ChileAtiende, call center 101, módulos express ChileAtiende, atención móvil y redes sociales. Los documentos almacenados pueden tratar de acceso a la salud pública, acceso a vivienda, inicio a estudios superiores, enfrentar la muerte de un familiar, iniciar la jubilación, quedar sin trabajo, ser migrante (vivir y trabajar en Chile), tener un hijo o hija o trámites por internet con Clave Única."
 
 # Dividir el texto en segmentos de no más de 512 tokens
 segment_size = 512
@@ -68,7 +67,10 @@ output = []
 for segment in segments:
     verb_noun_segment = extract_verbs_and_nouns(segment)
     output.extend(verb_noun_segment)
-    
-# Convertir el resultado en una cadena JSON
-json_output = json.dumps({"resultados": output}, ensure_ascii=False)
+
+  # Filtrar objetos no deseados
+filtered_output = [item for item in output if item['word'] != "[UNK]" and item['word'] != "[SEP]" and item['word'] != "[PAD]" and item['word'] != "[CLS]" and item['word'] != "[MASK]"]
+
+# Convertir el resultado filtrado en una cadena JSON
+json_output = json.dumps({"resultados": filtered_output}, ensure_ascii=False)
 print(json_output)
