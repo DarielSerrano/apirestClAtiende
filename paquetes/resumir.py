3d489398d85080a3a0cbe8f919efce43c4783b52
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-27 23:19:01 -0400

    s

--- paquetes/resumir.py
+++ paquetes/resumir.py
@@ -1,6 +1,6 @@
 import sys, json
 import re
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 
 # Funcion para leer archivos por ruta
 def file_get_contents(pathfile):
@@ -16,35 +16,9 @@ text = (file_get_contents(sys.argv[1]))
 # Definición de una función para manipular el texto (eliminar espacios y saltos de línea)
 WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 
-# Nombre del modelo a utilizar
-model_name = "csebuetnlp/mT5_multilingual_XLSum"
+text = WHITESPACE_HANDLER(text)
 
-# Cargar el tokenizador y el modelo
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-# Tokenizar el texto de entrada
-input_ids = tokenizer(
-    [WHITESPACE_HANDLER(text)],
-    return_tensors="pt",
-    padding="max_length",
-    truncation=True,
-    max_length=512
-)["input_ids"]
-
-output_ids = model.generate(
-    input_ids=input_ids,
-    max_length=400,
-    no_repeat_ngram_size=2,
-    num_beams=4
-)[0]
-
-# Decodificar el resumen generado en texto legible
-summary = tokenizer.decode(
-    output_ids,
-    skip_special_tokens=True,
-    clean_up_tokenization_spaces=False
-)
+summary = pipeline("summarization", model="csebuetnlp/mT5_multilingual_XLSum")
 
 # Realizar un reemplazo específico en el resumen generado
 summary = summary.replace("A continuación, ", "El siguiente documento trata de: ")
