cfbd56217e25099611f3096a9683ba40473f1e9d
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-28 15:38:32 -0400

    si

--- index.php
+++ index.php
@@ -11,7 +11,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 // pruebas en index usando post
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 {
+    // Ejecuta el script de Python y captura la salida
+    $python_output = exec('python3.10 paquetes/extraer.py archivos/prueba.txt');
 
+    // Decodifica la salida JSON en un array asociativo
+    $response_data = json_decode($python_output, true);
+
+    // Imprime la respuesta JSON en pantalla
+    echo json_encode($response_data, JSON_PRETTY_PRINT);
 }; 
 
 ?>
