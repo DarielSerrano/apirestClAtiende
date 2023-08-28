61e6c3a209b4947eeda6a3e2491f3af87bdff6c2
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-28 15:43:57 -0400

    si

--- index.php
+++ index.php
@@ -11,9 +11,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 // pruebas en index usando post
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 {
-    // Ejecuta el script de Python y captura la salida
-    $python_output = exec('python3.10 paquetes/extraer.py archivos/prueba.txt');
-
+    echo json_encode("Intentando ejecutar script python espere", JSON_PRETTY_PRINT);
+    try {
+        // Ejecuta el script de Python y captura la salida
+        $python_output = exec('python3.10 paquetes/extraer.py archivos/prueba.txt');
+        echo json_encode("\nScript ejecutado\n", JSON_PRETTY_PRINT);
+    } catch (\Throwable $th) {
+        echo json_encode("\nScript no ejecutado, error\n", JSON_PRETTY_PRINT);
+        $error = $th->getMessage();
+        echo json_encode($error, JSON_PRETTY_PRINT);
+    }
     // Decodifica la salida JSON en un array asociativo
     $response_data = json_decode($python_output, true);
 
