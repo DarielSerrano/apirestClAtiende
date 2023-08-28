9eaa28e103e8b01d10c17fa7a8834e88f4806e0c
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-28 19:46:17 -0400

    s

--- paquetes/extraer.py
+++ paquetes/extraer.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 # Importar el módulo 'sys' para manejar argumentos de línea de comandos
 import sys, json, stanza, re
 
-sys.path.append('/var/www/html/apirestClAtiende/librerias/')
+sys.path.append('/var/www/html/apirestClAtiende/librerias')
 
 stanza.download('es', package='ancora', processors='tokenize,mwt,pos,lemma', verbose=True) 
 stNLP = stanza.Pipeline(processors='tokenize,mwt,pos,lemma', lang='es', use_gpu=True) 
