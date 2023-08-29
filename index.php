1bbbf42c9c0b9653d46b6cdce48e40a38d4e027d
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-29 12:08:17 -0400

    analisis test in index

--- index.php
+++ index.php
@@ -143,22 +143,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 
                 //intentar ejecutar la aplicación pdftotext 
                 try {
-                    shell_exec ("cd /var/www/html/apirestClAtiende && pdftotext");
+                    $output = shell_exec ("cd /var/www/html/apirestClAtiende && pdftotext");
+                    if ($output === null) {
+                        throw new Exception("Hubo un problema al hacer la transformación de pdf a texto.");
+                    }
                 } 
-                catch (\Throwable $th) {
-                    $respuesta = "Hubo un problema al hacer la transformación de pdf a texto.";
+                catch (Exception $th) {
                     $error = $th->getMessage();
                     $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
                     $rutaLog = "logs_de_error.txt";                    
                     // Abre o crea el archivo de log en modo de escritura al final del archivo
                     $rutaLog = fopen($rutaLog, "a");
                     // Escribe la excepcion junto con la fecha y hora en el archivo de log
-                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    fwrite($rutaLog, "[$fechaHora]_$error" . PHP_EOL);
                     // Cierra el archivo de log
                     fclose($rutaLog);
                     header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
                     header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
-                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    echo json_encode($error, JSON_UNESCAPED_UNICODE);
                     exit;
                 }
 
