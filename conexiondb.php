0826e9c2c74d986e266a84cf226268123d7c37a0
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-29 11:24:23 -0400

    analisis test in index

--- conexiondb.php
+++ conexiondb.php
@@ -2,7 +2,7 @@
 include 'keysdb.php';
 
 // Crear la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $dbpassword, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) 
