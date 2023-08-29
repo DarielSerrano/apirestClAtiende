2ef1cae25792b1528d95a9f09797cb422efaa0e1
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-29 11:30:12 -0400

    analisis test in index

--- index.php
+++ index.php
@@ -1,42 +1,43 @@
 <?php
-include 'conexiondb.php';
-try {
-    // Consulta SQL con cláusula WHERE
-    $sql = "SELECT * FROM Usuario";
-    $result = $conn->query($sql);
-} 
-catch (\Throwable $th) {
-    $respuesta = "No se logró hacer la consulta a la base de datos para validar contraseña.";
-    $error = $th->getMessage();
-    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
-    $rutaLog = "logs_de_error.txt";                    
-    // Abre o crea el archivo de log en modo de escritura al final del archivo
-    $rutaLog = fopen($rutaLog, "a");
-    // Escribe la excepcion junto con la fecha y hora en el archivo de log
-    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
-    // Cierra el archivo de log
-    fclose($rutaLog);
-    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
-    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
-    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
-    exit;
-}        
-
-if ($result->num_rows > 0) {
-    // Encontrado, procesa los resultados
-    while ($row = $result->fetch_assoc()) {
-        // Accede a los valores en $row
-        echo json_encode($row, JSON_UNESCAPED_UNICODE);        
-    }
-} 
-// Cerrar la conexión
-$conn->close();  
+ 
 //codigo inicial del metodo get 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') 
 {
     header("HTTP/1.1 200 OK");
     $respuesta = "Api de analisis, busqueda y preguntas frecuentes";
     echo json_encode ($respuesta); 
+    include 'conexiondb.php';
+    try {
+        // Consulta SQL con cláusula WHERE
+        $sql = "SELECT * FROM Usuario";
+        $result = $conn->query($sql);
+    } 
+    catch (\Throwable $th) {
+        $respuesta = "No se logró hacer la consulta a la base de datos para validar contraseña.";
+        $error = $th->getMessage();
+        $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+        $rutaLog = "logs_de_error.txt";                    
+        // Abre o crea el archivo de log en modo de escritura al final del archivo
+        $rutaLog = fopen($rutaLog, "a");
+        // Escribe la excepcion junto con la fecha y hora en el archivo de log
+        fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+        // Cierra el archivo de log
+        fclose($rutaLog);
+        header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+        header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+        echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+        exit;
+    }        
+    
+    if ($result->num_rows > 0) {
+        // Encontrado, procesa los resultados
+        while ($row = $result->fetch_assoc()) {
+            // Accede a los valores en $row
+            echo json_encode($row, JSON_UNESCAPED_UNICODE);        
+        }
+    } 
+    // Cerrar la conexión
+    $conn->close();     
 }; 
 
 /* if ($_SERVER['REQUEST_METHOD'] == 'POST') {
