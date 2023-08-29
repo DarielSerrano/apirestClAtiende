1afdb222daec7ad5e1df7e66e1c46d16367adcb7
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-29 13:16:30 -0400

    analisis test in index

--- index.php
+++ index.php
@@ -177,7 +177,225 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
                     exit;
                 }
+                
+                // Inicio creacion automatica de etiqueta y guardado en BD
+                try {
+                    $output = array();
+                    $return_var = 0;
+                    $errcapture = "2>&1";
+                    $etiquetar = "cd /var/www/html/apirestClAtiende && python3.10 paquetes/etiquetar.py";
+                    // Ejecutar el comando y capturar la salida en $output y el estado de retorno en $return_var
+                    exec("$etiquetar $ruta_txt $errcapture", $output, $return_var);
+                    
+                    // Verificar el estado de retorno para determinar si hubo un error
+                    if ($return_var === 0) {
+                        // El comando se ejecutó correctamente
+                        $respuesta = implode("\n", $output); // La salida del comando
+                        header("HTTP/1.1 200 OK");
+                        header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                        echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);  
+                       
+                    } else {
+                        // Hubo un error al ejecutar el comando
+                        $error_message = implode("\n", $output); // Los mensajes de error generados
+                        throw new Exception($error_message);
+                    }
+                } 
+                catch (Exception $th) {
+                    $respuesta = "Mensaje error";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }
+                
+                // 
+                
+                try { // Inicio creacion automatica de etiqueta
+                    header("HTTP/1.1 200 OK");
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($salida, JSON_UNESCAPED_UNICODE);                                                                   
+                }
+                  
+                try { // guardado en BD
+                    //code...
+                } 
+                catch (\Throwable $th) {
+                    $respuesta = "Hubo un problema al guardar la etiqueta.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }
+                
 
+                // Inicio creacion del documento y guardado en BD
+                try {
+                    $nombreDocumento;
+                } 
+                catch (\Throwable $th) {
+                    //throw $th;
+                }
+
+
+                // Inicio extraccion palabras clave NLP y guardado en BD
+                $extraer = "cd /var/www/html/apirestClAtiende && TRANSFORMERS_CACHE=cache STANZA_RESOURCES_DIR=stanza_resources python3.10 paquetes/extraer.py";
+                try {
+                    //ejecucion extraccion NLP
+                    $salida = shell_exec("$extraer $ruta_txt");
+                    header("HTTP/1.1 200 OK");
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($salida, JSON_UNESCAPED_UNICODE);                                                                       
+                }
+                catch (\Throwable $th) {
+                    $respuesta = "Hubo un problema al hacer la extracción NLP.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }                    
+                try { // guardado en BD
+                    /* // Inicializar los contadores
+                    $frecuenciaVerbos = [];
+                    $frecuenciaSustantivos = [];
+
+                    // Iterar sobre los resultados y contar las repeticiones
+                    foreach ($resultados as $resultado) {
+                        $palabra = $resultado["word"];
+                        $entidad = $resultado["entity"];
+                        
+                        if ($entidad === "VERB") {
+                            if (!array_key_exists($palabra, $frecuenciaVerbos)) {
+                                $frecuenciaVerbos[$palabra] = 1;
+                            } else {
+                                $frecuenciaVerbos[$palabra]++;
+                            }
+                        } elseif ($entidad === "NOUN" || $entidad === "PROPN") {
+                            if (!array_key_exists($palabra, $frecuenciaSustantivos)) {
+                                $frecuenciaSustantivos[$palabra] = 1;
+                            } else {
+                                $frecuenciaSustantivos[$palabra]++;
+                            }
+                        }
+                    }
+
+                    // Ordenar verbos por frecuencia de mayor a menor
+                    arsort($frecuenciaVerbos);
+
+                    // Tomar los primeros 20 verbos y sus frecuencias
+                    $top20Verbos = array_slice($frecuenciaVerbos, 0, 20, true);
+
+                    // Ordenar sustantivos por frecuencia de mayor a menor
+                    arsort($frecuenciaSustantivos);
+
+                    // Tomar los primeros 30 sustantivos y sus frecuencias
+                    $top30Sustantivos = array_slice($frecuenciaSustantivos, 0, 30, true);
+
+                    include 'conexiondb.php';
+
+                    // Insertar frecuencias de verbos
+                    foreach ($top20Verbos as $verbo => $frecuencia) {
+                        $sql = "INSERT INTO Verbos (VerbosNombre, VerbosFrecuencia) VALUES ('$verbo', $frecuencia)";
+                        $conn->query($sql);
+                    }
+
+                    // Insertar frecuencias de sustantivos (PROPN y NOUN juntos)
+                    foreach ($top30Sustantivos as $sustantivo => $frecuencia) {
+                        $sql = "INSERT INTO Sustantivos (SustantivosNombre, SustantivosFrecuencia) VALUES ('$sustantivo', $frecuencia)";
+                        $conn->query($sql);
+                    }
+
+                    // Cerrar la conexión
+                    $conn->close(); */
+
+                } 
+                catch (\Throwable $th) {
+                    $respuesta = "Hubo un problema al guardar la extraccion.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }
+                
+                // Inicio creacion del resumen de documento NLP y guardado en BD
+                $resumir = "cd /var/www/html/apirestClAtiende && TRANSFORMERS_CACHE=cache python3.10 paquetes/resumir.py";
+                try { // Inicio creacion automatica de etiqueta
+                    $salida = shell_exec("$resumir $ruta_txt");
+                    header("HTTP/1.1 200 OK");
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($salida, JSON_UNESCAPED_UNICODE);
+                } 
+                catch (\Throwable $th) {
+                    $respuesta = "Hubo un problema al generar el resumen.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }
+                try { // guardado en BD
+                    
+                } 
+                catch (\Throwable $th) {
+                    $respuesta = "Hubo un problema al guardar el resumen.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }                
                 
             }
             else {
@@ -297,6 +515,82 @@ function validarContrasena($rut,$pass) {
         return false;
     }
 
+}
+
+//intentar ejecutar la aplicación pdftotext 
+                try {
+                    $output = array();
+                    $return_var = 0;
+                    $errcapture = "2>&1";
+
+                    // Ejecutar el comando y capturar la salida en $output y el estado de retorno en $return_var
+                    exec("cd /var/www/html/apirestClAtiende && pdftotext $ruta_pdf $ruta_txt $errcapture", $output, $return_var);
+
+                    // Verificar el estado de retorno para determinar si hubo un error
+                    if ($return_var === 0) {
+                        /* // El comando se ejecutó correctamente
+                        $response = implode("\n", $output); // La salida del comando
+                        echo "Comando ejecutado exitosamente:\n$response"; */
+                    } else {
+                        // Hubo un error al ejecutar el comando
+                        $error_message = implode("\n", $output); // Los mensajes de error generados
+                        throw new Exception($error_message);
+                    }
+                } 
+                catch (Exception $th) {
+                    $respuesta = "Hubo un problema al hacer la transformación de pdf a texto.";
+                    $error = $th->getMessage();
+                    $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+                    $rutaLog = "logs_de_error.txt";                    
+                    // Abre o crea el archivo de log en modo de escritura al final del archivo
+                    $rutaLog = fopen($rutaLog, "a");
+                    // Escribe la excepcion junto con la fecha y hora en el archivo de log
+                    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+                    // Cierra el archivo de log
+                    fclose($rutaLog);
+                    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+                    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+                    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+                    exit;
+                }
+
+function plantilla_try_catch() {
+        // plantilla try catch
+        try {
+            $output = array();
+            $return_var = 0;
+            $errcapture = "2>&1";
+
+            // Ejecutar el comando y capturar la salida en $output y el estado de retorno en $return_var
+            exec("cd /var/www/html/apirestClAtiende && pdftotext $errcapture", $output, $return_var);
+
+            // Verificar el estado de retorno para determinar si hubo un error
+            if ($return_var === 0) {
+                /* // El comando se ejecutó correctamente
+                Code... 
+                */
+            } else {
+                // Hubo un error al ejecutar el comando
+                $error_message = implode("\n", $output); // Los mensajes de error generados
+                throw new Exception($error_message);
+            }
+        } 
+        catch (Exception $th) {
+            $respuesta = "Mensaje error";
+            $error = $th->getMessage();
+            $fechaHora = preg_replace('/\s+/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+            $rutaLog = "logs_de_error.txt";                    
+            // Abre o crea el archivo de log en modo de escritura al final del archivo
+            $rutaLog = fopen($rutaLog, "a");
+            // Escribe la excepcion junto con la fecha y hora en el archivo de log
+            fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+            // Cierra el archivo de log
+            fclose($rutaLog);
+            header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+            header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+            echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+            exit;
+        }
 }
 
 ?>
