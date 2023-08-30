cabf504bec9aa8bd57279a64f951d687337d7b0b
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-30 16:17:09 -0400

    intento json con respuestas fix

--- buscar.php
+++ buscar.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         } 
         catch (Exception $th) {
-            $respuesta = "Hubo un problema al hacer la extracción NLP.";
+            $respuesta = "Hubo un problema al hacer la extracción NLP a la busqueda.";
             $error = $th->getMessage();
             $fechaHora = preg_replace('/\s/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
             $rutaLog = "logs_de_error.txt";                    
@@ -75,6 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             exit;
         }  
         try {
+            include 'conexiondb.php';
             $verbos = implode("', '", $verb); // Unir verbos en formato 'verbo1', 'verbo2', ...
             $sustantivos = implode("', '", $sustantivo); // Unir sustantivos en formato 'sustantivo1', 'sustantivo2', ...
             $sql = 
@@ -121,7 +122,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             ORDER BY
                 TotalFrecuencia DESC
             LIMIT 5;";
-            
+
             if ($resultado_consulta = $conn->query($sql)) {
                 // Crear un arreglo para almacenar los resultados modificados
                 $documentos_modificados = array();
@@ -137,9 +138,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 // Imprimir el JSON
                 echo $json_resultado;
-            }
+            } 
+            else {
+                // Manejar error en la consulta de inserción
+                $error_message = $conn->error; // Mensaje de error generado
+                // Cerrar la conexión
+                $conn->close();
+                throw new Exception($error_message);
+            } 
+            // Cerrar la conexión
+            $conn->close();
+
         } catch (\Throwable $th) {
-            //throw $th;
+            $respuesta = "Hubo un problema intentar la busqueda en el sistema.";
+            $error = $th->getMessage();
+            $fechaHora = preg_replace('/\s/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
+            $rutaLog = "logs_de_error.txt";                    
+            // Abre o crea el archivo de log en modo de escritura al final del archivo
+            $rutaLog = fopen($rutaLog, "a");
+            // Escribe la excepcion junto con la fecha y hora en el archivo de log
+            fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+            // Cierra el archivo de log
+            fclose($rutaLog);
+            header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+            header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+            echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+            exit;
         }                                
                                
         
