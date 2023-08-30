9aa229088389bc35fb8ee8f4ab8805f39d374c53
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-30 14:01:20 -0400

    update fix

--- analisis.php
+++ analisis.php
@@ -278,7 +278,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 try { // Inicio busqueda de ids para creacion documento
                     include 'conexiondb.php';                                                        
                     // Consulta SQL con cláusula WHERE
-                    $sql = "SELECT idDocumentosCategoria FROM DocumentosCategoria WHERE DocumentosCategoriaNombre = $dbetiqueta";                    
+                    $sql = "SELECT idDocumentosCategoria FROM DocumentosCategoria WHERE DocumentosCategoriaNombre = '$dbetiqueta'";                    
                     $sql = preg_replace('/[^A-Za-z.,()\s_\'"$]/', '', $sql); //asegurar solo caracteres propios de la consulta hecha
                     echo json_encode($sql, JSON_UNESCAPED_UNICODE);
                     // Ejecutar la consulta                 
