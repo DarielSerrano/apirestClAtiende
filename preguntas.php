98b7d2fa6ea50b061eecb4a30a7ab94460723b6f
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-31 01:51:30 -0400

    fixes

--- preguntas.php
+++ preguntas.php
@@ -1,5 +1,12 @@
 <?php
 
+//codigo inicial del metodo get 
+if ($_SERVER['REQUEST_METHOD'] == 'GET') 
+{
+    header("HTTP/1.1 200 OK");
+    $respuesta = "Api de análisis, búsqueda y preguntas frecuentes.";
+    echo json_encode ($respuesta);      
+}; 
 
 
 ?>
