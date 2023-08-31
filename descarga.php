2f3de192608fcf97cf523f68ac02aa4b0f403846
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-30 21:07:06 -0400

    ulr fix buscar

--- descarga.php
+++ descarga.php
@@ -6,7 +6,7 @@ if (isset($_GET['archivo'])) {
     // Ruta real del archivo en el servidor
     $rutaArchivo = '/var/www/html/apirestClAtiende/' . $archivo;
     $rutaArchivo = preg_replace('/\\+$/','',$rutaArchivo);
-
+    echo ($rutaArchivo);
     // Verifica si el archivo existe y es accesible
     if (file_exists($rutaArchivo)) {
         // Configura las cabeceras para forzar la descarga
