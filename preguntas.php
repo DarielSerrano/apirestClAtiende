0e0d738acc9fe0592d1bb505d1c9976e02dad006
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-31 07:27:08 -0400

    preguntas PUT

--- preguntas.php
+++ preguntas.php
@@ -405,7 +405,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
             // Cerrar la conexión
             $conn->close();
         } catch (\Throwable $th) {
-            $respuesta = "Pruebe con una de las categorias definidas.";
+            $respuesta = "La actualización de datos falló.";
             $error = $th->getMessage();
             $fechaHora = preg_replace('/\s/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
             $rutaLog = "logs_de_error.txt";                    
@@ -420,40 +420,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
             echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
             exit;
         }
-
-        try {
-            include 'conexiondb.php';                                                        
-            // Consulta SQL
-            $sql = "INSERT INTO Preguntas(idPreguntasFrecuentes, PreguntasPreguntaFrecuente, PreguntasRespuesta, DocumentosCategoria_idDocumentosCategoria) VALUES (NULL,'$preguntaFrec','$respuestaFrec',$categoriaID)";                    
-            // Limpieza ante posibles inyecciones
-            $sql = preg_replace('/[^0-9A-Za-z\s(),\'\":._\-$+=\*]/',"",$sql);
-            // Ejecutar la consulta                 
-            if ($result = $conn->query($sql)) {
-                // Consulta exitosa
-            } else {
-                // Manejar error en la consulta
-                $error_message = $conn->error; // Mensaje de error generado
-                $conn->close(); 
-                throw new Exception($error_message);
-            } 
-            // Cerrar la conexión
-            $conn->close();
-        } catch (\Throwable $th) {
-            $respuesta = "Hubo un problema con la creacion de la pregunta y respuesta en el sistema.";
-            $error = $th->getMessage();
-            $fechaHora = preg_replace('/\s/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                        
-            $rutaLog = "logs_de_error.txt";                    
-            // Abre o crea el archivo de log en modo de escritura al final del archivo
-            $rutaLog = fopen($rutaLog, "a");
-            // Escribe la excepcion junto con la fecha y hora en el archivo de log
-            fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
-            // Cierra el archivo de log
-            fclose($rutaLog);
-            header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
-            header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
-            echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
-            exit;
-        } 
     }
 } 
 ?>
