f89984de8597be823f9f3cc5d049104df2de7d35
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-30 23:53:46 -0400

    descarga fix buscar

--- descarga.php
+++ descarga.php
@@ -6,30 +6,21 @@ if (isset($_GET['doc'])) {
     $rutaArchivo = $directorioPermitido . $nombreArchivoReal;
 
     // Verifica si la ruta del archivo está dentro del directorio permitido
-    if (strpos(realpath($rutaArchivo), $directorioPermitido) === 0) {
-        if (file_exists($rutaArchivo)) {            
-            // Configura las cabeceras para forzar la descarga
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="' . basename($rutaArchivo) . '"');
-            header('Content-Length: ' . filesize($rutaArchivo));
+    if (strpos(realpath($rutaArchivo), $directorioPermitido) === 0 && file_exists($rutaArchivo)) {
+        // Configura las cabeceras para forzar la descarga
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($rutaArchivo) . '"');
+        header('Content-Length: ' . filesize($rutaArchivo));
 
-            // Envía el contenido del archivo
-            readfile($rutaArchivo);
-            exit;
-        }
-        else {
-            header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
-            header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
-            echo json_encode('Archivo no encontrado.', JSON_UNESCAPED_UNICODE);
-        }
-    } 
-    else {
+        // Envía el contenido del archivo
+        readfile($rutaArchivo);
+        exit;
+    } else {
         header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
         header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
-        echo json_encode('Archivo no permitido para descarga.', JSON_UNESCAPED_UNICODE);
+        echo json_encode('Archivo no existe o no esta permitido para descarga.', JSON_UNESCAPED_UNICODE);
     }
-} 
-else {
+} else {
     header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
     header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
     echo json_encode('Archivo no especificado.', JSON_UNESCAPED_UNICODE);
