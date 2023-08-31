993409a6a6ef4de8dbcdd4640432def70fe04383
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-08-30 21:18:33 -0400

    ulr fix buscar

--- descarga.php
+++ descarga.php
@@ -5,22 +5,23 @@ if (isset($_GET['archivo'])) {
 
     // Ruta real del archivo en el servidor
     $rutaArchivo = '/var/www/html/apirestClAtiende/' . $archivo;
+
     // Verifica si el archivo existe y es accesible
-    if (is_file($rutaArchivo)) {
-         // Configura las cabeceras para forzar la descarga
-         header('Content-Description: File Transfer');
-         header('Content-Type: application/octet-stream');
-         header('Content-Disposition: attachment; filename="' . basename($rutaArchivo) . '"');
-         header('Expires: 0');
-         header('Cache-Control: must-revalidate');
-         header('Pragma: public');
-         header('Content-Length: ' . filesize($rutaArchivo));
- 
-         // Lee y envía el archivo
-         readfile($rutaArchivo);
-         exit;
+    if (file_exists($rutaArchivo)) {
+        // Configura las cabeceras para forzar la descarga
+        header('Content-Type: application/force-download');
+        header('Content-Disposition: attachment; filename=' . basename($rutaArchivo));
+        header('Content-Length: ' . filesize($rutaArchivo));
+
+        // Lee y envía el archivo en bloques
+        $fp = fopen($rutaArchivo, 'rb');
+        while (!feof($fp)) {
+            echo fread($fp, 1024);
+        }
+        fclose($fp);
+        exit;
     } else {
-        echo 'El archivo no existe o es un directorio.';
+        echo 'El archivo no existe.';
     }
 } else {
     echo 'Archivo no especificado.';
