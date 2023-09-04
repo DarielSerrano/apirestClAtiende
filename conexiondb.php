ff926e18f7707b5d0e978dd9c6bc8b3f41faef34
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2023-09-03 22:52:45 -0300

    prueba regex

--- conexiondb.php
+++ conexiondb.php
@@ -5,16 +5,33 @@ include 'keysdb.php';
 // Crear la conexión
 $conn = new mysqli($servername, $username, $dbpassword, $dbname);
 
-// Verificar la conexión
-if ($conn->connect_error) 
-{
-    die("Conexión fallida: " . $conn->connect_error);
+try {
+    if ($conn->connect_error) {
+        // Manejar error en la consulta de inserción
+        $error_message = $conn->error; // Mensaje de error generado
+        // Cerrar la conexión
+        $conn->close();
+        throw new Exception($error_message);
+    }
+    else {
+        // Conexion exitosa
+    }
 }
-
-/* Ejemplo de consulta 
-$sql = "SELECT * FROM nombre_de_tabla";
-
-$result = $conn->query($sql);  
-*/
+catch (\Throwable $th) {
+    $respuesta = "Hubo un problema interno en el servidor.";
+    $error = $th->getMessage();
+    $fechaHora = preg_replace('/\s/', '_', date("Y-m-d H:i:s")); // Obtiene la fecha y hora actual                                      
+    $rutaLog = "logs_de_error.txt";                    
+    // Abre o crea el archivo de log en modo de escritura al final del archivo
+    $rutaLog = fopen($rutaLog, "a");
+    // Escribe la excepción junto con la fecha y hora en el archivo de log
+    fwrite($rutaLog, "[$fechaHora]($respuesta)_$error" . PHP_EOL);
+    // Cierra el archivo de log
+    fclose($rutaLog);
+    header("HTTP/1.1 400 Bad Request");  // Encabezado de estado
+    header('Content-Type: application/json; charset=UTF-8');  // Encabezado Content-Type
+    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+    exit;
+}
 
 ?>
--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 //código inicial del método get
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-    header("HTTP/1.1 200 OK");
+    /* header("HTTP/1.1 200 OK");
     $respuesta = "Api de análisis, búsqueda y preguntas frecuentes.";
-    echo json_encode ($respuesta);      
+    echo json_encode ($respuesta);  */     
+    header('Content-Type: application/json; charset=UTF-8');
+    $namefinal = preg_replace('/([^0-9A-Za-zÑñ\s(),?¿¡!\'\":._\-$+=\*@#%º])/', '', $_GET['prueba']); 
+    $namefinal = correccion_tildes($namefinal);
+    echo json_encode($namefinal, JSON_UNESCAPED_UNICODE);
+    
+    function correccion_tildes($text) {
+        // Mapeo de caracteres con tilde a sus versiones sin tilde
+        $replace_map = array(
+            'á' => 'a', 'é' => 'e', 'í' => 'i', 'ó' => 'o', 'ú' => 'u',
+            'Á' => 'A', 'É' => 'E', 'Í' => 'I', 'Ó' => 'O', 'Ú' => 'U',
+        );
+        
+        // Reemplazar los caracteres con tilde por sus versiones sin tilde
+        $updated_text = strtr($text, $replace_map);
+        
+        return $updated_text;
+    }    
 };
 
 ?>
